c9fd844436678b8475b8c6332c0e583de2f18331
Laura Petrich <laura.petrich.gmail.com>
2021-04-07 21:17:06 -0600

    updated competition2 server

--- competition2/src/competition2_server.py
+++ competition2/src/competition2_server.py
@@ -4,29 +4,33 @@ import rospy
 from std_srvs.srv import Empty, EmptyResponse 
 from competition2.srv import BanditStep, BanditStepResponse, BanditAnswer, BanditAnswerResponse
 from competition2.srv import ShapesAnswer, ShapesAnswerResponse
+from competition2.srv import MazeAnswer, MazeAnswerResponse
 import numpy as np
 
-shapes_count = rospy.get_param('/competition2_server/shapes_count', 1)
-shapes_type = rospy.get_param('/competition2_server/shapes_type', "blue cube")
-shapes_room_a = rospy.get_param('/competition2_server/shapes_room_a', 1)
-shapes_room_b = rospy.get_param('/competition2_server/shapes_room_b', 2)
+shapes_count = rospy.get_param('/competition2_server/shapes_count', 6)
+shapes_type = rospy.get_param('/competition2_server/shapes_type', "red cube")
+shapes_room = rospy.get_param('/competition2_server/shapes_room', 19)
 
-# Parameters for bandit room
-bandit_room_a = rospy.get_param('/competition2_server/bandit_room_a', 1)
-bandit_room_b = rospy.get_param('/competition2_server/bandit_room_b', 2)
+bandit_room = rospy.get_param('/competition2_server/bandit_room', 6)
 bandit_passcode = rospy.get_param('/competition2_server/bandit_passcode', 42)
 bandit_num_arms = rospy.get_param('/competition2_server/bandit_num_arms', 2)
 bandit_probs = rospy.get_param('/competition2_server/bandit_probs', "0.8, 0.5")
 bandit_probs = [float(i) for i in bandit_probs.split(',')]
 bandit_max_idx = bandit_probs.index(max(bandit_probs))
 
+maze_room = rospy.get_param('/competition2_server/maze_room', 3)
+maze_passcode = rospy.get_param('/competition2_server/maze_passcode', 37)
+
+final_room = rospy.get_param('/competition2_server/final_room', 8)
+
 def shapes_answer_callback(request):
 	count = request.count
 	response = ShapesAnswerResponse()
+	response.room = bandit_room
 	if count == shapes_count:
-		response.room = shapes_room_a
+		response.how = "Revolver"
 	else:
-		response.room = shapes_room_b
+		response.how = ""
 	return response
 
 def sample_reward(action):
@@ -36,7 +40,7 @@ def sample_reward(action):
 def bandit_step_callback(request):
 	action = request.action - 1
 	response = BanditStepResponse()
-	if request.code != bandit_passcode or action < 0 or action >= bandit_num_arms:
+	if request.passcode != bandit_passcode or action < 0 or action >= bandit_num_arms:
 		response.valid = False
 		response.reward = -np.inf
 	else:
@@ -47,18 +51,29 @@ def bandit_step_callback(request):
 def bandit_answer_callback(request):
 	arm = request.arm - 1
 	response = BanditAnswerResponse()
+	response.room = maze_room
 	if arm == bandit_max_idx:
-		response.room = bandit_room_a
+		response.where = "Lounge"
 	else:
-		response.room = bandit_room_b
+		response.where = ""
+	return response
+
+def maze_answer_callback(request):
+	response = MazeAnswerResponse()
+	response.room = final_room
+	if request.passcode == maze_passcode:
+		response.who = "Dr. Taylor"
+	else:
+		response.who = ""
 	return response
 
 rospy.init_node('competition2_server') 
 
 shapes_answer_srv = rospy.Service('/shapes_answer', ShapesAnswer, shapes_answer_callback)
 
-# Servives for bandit room
 bandit_step_srv = rospy.Service('/bandit_step', BanditStep, bandit_step_callback)
 bandit_answer_srv = rospy.Service('/bandit_answer', BanditAnswer, bandit_answer_callback)
+
+maze_answer_srv = rospy.Service('/maze_answer', MazeAnswer, maze_answer_callback)
 
 rospy.spin()
